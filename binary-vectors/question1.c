6c504fecc75a06427226d1d0e5e01254735820a1
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-03-12 22:12:21 +0530

    Adding binary vector

---
+++ binary-vectors/question1.c
@@ -0,0 +1,55 @@
+/* Written by Giri
+   A function taking as inputs an integer u 
+   (viewed as a binary vector) and an integer i,
+   which returns the i-th bit of u	*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+void check_first_bit ( unsigned int bit ) {
+	printf ("The first bit is : %d\n", bit&1);
+}
+
+int check_random_bit ( unsigned int bit, int i ) {
+	int temp = ( bit >> i ) &	1;
+	return temp;
+}
+
+int main ( int argc, char ** argv ) {
+	unsigned int u;
+	int i, choice;
+	do {
+		printf ( "=====================================================\n");
+		printf ( "1. Check first bit\n" );
+		printf ( "2. Check a random bit\n");
+		printf ( "3. Exit\n");
+
+		printf ( "Enter your choice: ");
+		scanf ("%d", &choice);
+
+		printf ("Enter the integer: ");
+		scanf ("%du", &u);
+
+		switch ( choice ) {
+			case 1: check_first_bit (u);
+			break;
+
+			case 2:	printf ("Enter the value of ith bit which you want to extract from the integer: ");
+					scanf ("%d", &i);
+					printf ("The requested bit is : %d\n" , check_random_bit(u, i));
+			break;
+
+			case 3: printf ( "\n=====================================================\n");
+					exit (0);
+			break;
+
+			default: printf ("Wrong choice!!\n");
+			break;
+		}
+		printf ( "=====================================================\n");
+	} while (choice != 0 || choice > 3 || choice < 0);
+
+}
