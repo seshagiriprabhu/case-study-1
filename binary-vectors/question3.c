09e803a2dda927cf32fa1be498c007d40d7d366a
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-03-12 22:51:34 +0530

    Adding question3

---
+++ binary-vectors/question3.c
@@ -0,0 +1,32 @@
+/* Written by Giri
+   A function that implements the Hamming 
+   weight of a binary vector */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char ** argv ) {
+	unsigned int u;
+	int choice;
+	do {
+		printf ( "==========================================\n");
+		printf ("1. Find the Hamming weight of a binary vector: \n");
+		printf ("2. Exit\n");
+		printf ("Enter your choice: ");
+		scanf ("%d", &choice);
+		switch ( choice ) {
+			case 1: printf ("Enter the binary vector: ");
+					scanf ("%du", &u);
+					printf ("Number of 1s in your binary vector is : %d\n", __builtin_popcount(u));
+			break;
+			
+			case 2: printf ( "==========================================\n");
+					exit(0);
+			break;
+
+			default:
+			break;
+		}
+		printf ( "==========================================\n");
+	} while ( choice < 1 || choice > 2 );
+}
