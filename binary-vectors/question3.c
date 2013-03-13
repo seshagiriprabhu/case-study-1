36ab8b3e2adfcc2de4ad4bdedbcc648332db663e
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-03-13 12:02:41 +0530

    Fixed the menu display bug

--- binary-vectors/question3.c
+++ binary-vectors/question3.c
@@ -8,22 +8,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned int count_non_zero ( unsigned int u ) {
+	unsigned int count = 0, i;
+
+	for ( i = 0; i < sizeof (u); i++ ) {
+		if ( ( ( u >> i ) & 1)  == 1 )
+			count++;
+	}
+
+	return count;
+}
+
 int main ( int argc, char ** argv ) {
 	unsigned int u;
 	int choice;
 	do {
 		printf ( "==========================================\n");
-		printf ("1. Find the Hamming weight of a binary vector: \n");
-		printf ("2. Exit\n");
+		printf ("1. Find the Hamming weight of a binary vector (using built in function): \n");
+		printf ("2. Find the Hamming weight of a binary vector (using a custom function): \n");
+		printf ("3. Exit\n");
 		printf ("Enter your choice: ");
 		scanf ("%d", &choice);
 		switch ( choice ) {
 			case 1: printf ("Enter the binary vector: ");
 					scanf ("%du", &u);
-					printf ("Number of 1s in your binary vector is : %d\n", __builtin_popcount(u));
+					printf ("The Number of 1s in your binary vector is : %d\n", __builtin_popcount(u));
 			break;
 			
-			case 2: printf ( "==========================================\n");
+			case 2: printf ("Enter the binary vector: ");
+					scanf ("%du", &u);
+
+					if ( u == 0 )
+						printf ("There isn't any non-zero values in your binary vector\n");
+					else
+						printf ("The Number of 1s in your binary vector is : %d\n", count_non_zero(u));
+			break;
+
+			case 3: printf ( "==========================================\n");
 					exit(0);
 			break;
 
@@ -31,5 +52,6 @@ int main ( int argc, char ** argv ) {
 			break;
 		}
 		printf ( "==========================================\n");
-	} while ( choice < 1 || choice > 2 );
+	} while ( ! ( choice < 1 || choice > 2 ) );
+	return  0;
 }
