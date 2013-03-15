05c76175955b9dfe51443fa96f9868d98bac76df
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-03-15 15:22:53 +0530

    Made changes in the display

--- boolean-functions/question4.c
+++ boolean-functions/question4.c
@@ -1,10 +1,26 @@
 /* Written by Giri
-    */
+   A function taking as input a truthtable (as an array 
+   of ints), and returning its Hamming weight */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ( int argc, char ** argv ) {
+int calculate_total_hamming_weight ( unsigned int * val ) {
+	int i, total_weight = 0;
 
+	for ( i = 0; i < 4; i++ ) {
+		total_weight += __builtin_popcount(val[i]);
+	}
+	return total_weight;
+}
+
+int main ( int argc, char ** argv ) {
+	unsigned int table_f[4];
+	int i;
+	for ( i = 0; i < 4; i++) {
+		printf ("Enter the %d(st/nd/rd/th) boolean value: ", i + 1);
+		scanf ("%du", &table_f[i]);
+	}
+	printf ("The hamming weight of the given boolean function is : %d\n", calculate_total_hamming_weight(table_f));
 	return 0;
 }
