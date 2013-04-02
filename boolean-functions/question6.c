69badb59144c91f1a65194798e74993958a63c36
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-04-02 18:35:33 +0530

    Added a hamming function and fixed the bug related to m

--- boolean-functions/question6.c
+++ boolean-functions/question6.c
@@ -8,11 +8,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* A function to calculate the hamming weight */
+int hamming_weight (unsigned value) {
+	int count = 0;
+
+	while ( value > 0 ) {
+		if ( ( value & 1 ) == 1 ) 
+			count = count + 1;
+		value = value >> 1;
+	}
+
+	return count;
+}
+
+/* A function which check whether the boolean functions are balanced
+ * or not */
 void is_balanced (unsigned int * f, int m) {
 	int i, count = 0;
 
 	for ( i = 0; i < m; i++) {
-		count += __builtin_popcount (f[i]);
+		count += hamming_weight (f[i]);
 	}
 
 	if ( count != ( m / 2 ) )
@@ -27,18 +42,21 @@ int main ( int argc, char ** argv ) {
 	int m, i;
 	printf ("Enter the value of m: ");
 	scanf ("%d", &m);
+	
+	if ( m == 0) {
+		printf ("You must atleast give a value\n");
+		exit(0);
+	}
+
 	f = ( unsigned int * ) malloc ( m * sizeof ( unsigned int ) );
+
 
 	for ( i = 0; i < m; i++ ) {
 		printf ("Enter the value of bool[%d]: ", i + 1);
 		scanf ("%du", &f[i]);
 	}
 
-	if ( m == 0)
-		printf ("You must atleast give a value\n");
-
-	else
-		is_balanced (f, m);
+	is_balanced (f, m);
 
 	return 0;
 }
