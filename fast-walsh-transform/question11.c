be4d05575d16ffdff32f82a8f4d9e78e248b4b6f
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2013-04-03 14:44:01 +0530

    Completely implemented the fast walsh transformation

--- fast-walsh-transform/question11.c
+++ fast-walsh-transform/question11.c
@@ -5,47 +5,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void FastWalshTransform (int * f, int m ) {
-	int *temp_f;
-	temp_f = ( int * ) malloc ( pow(2, m) * sizeof(int) );
-	int u, temp = m;
+/* A custom function to find the power of a given number */
+int powerFunction ( int number, int power ) {
+	if ( power == 0 )
+		return 1;
 
-	for ( u = 0; u < pow (2, m); u++ ) {
+	number = number << (power - 1 );
+	return number;
+}
+
+/* A function to computer the FastWalshTransform */
+void FastWalshTransform ( int * f, int m ) {
+	int * temp_f;
+	temp_f = ( int * ) malloc ( powerFunction (2, m) * sizeof(int) );
+	int u, j, temp = m, count = 0;
+	int split, tempSplit;
+	
+	/* Prints the actual boolean values as index */
+	printf ("\nm\t|\t");
+	for (u = 0; u < powerFunction (2, m); u++ ) 
+		printf ("%d\t", f[u]);
+	printf ("\n--------------------------------------------------------------------------------\n");
+	
+	/* Replaces all the values of the function by their sign */
+	for ( u = 0; u < powerFunction (2, m); u++ ) {
+
+		if ( f[u] == 0 ) 
+			f[u] = 1;
+	
+		else 
+			f[u] = -1;
+
 		temp_f[u] = f[u];
 	}
 
-	int power  = pow (2, m);
-	int tempPower;
-	printf ("\nm\t|\t");
-	for (u = 0; u < pow (2, m); u++ ) 
+	/* Prints the initial reverse sign boolean values */
+	printf ("%d\t|\t", temp);
+	for ( u = 0; u < powerFunction (2, m); u++ ) {
 		printf ("%d\t", f[u]);
-	printf ("\n--------------------------------------------------------------------------------\n");
 
-	while ( temp > 0 ) {
-		tempPower = pow (2, temp - 1 );		
+		if (u == powerFunction (2, m) - 1 )
+			printf ("\n");
+	}
+
+	temp = temp - 1;
+	while ( temp > -1 ) {	
+		split = powerFunction (2, m) / ( powerFunction (2, m) / powerFunction (2, temp) );
+		tempSplit = split;
+
 		printf ("%d\t|\t", temp);	
 
-		for ( u = 0; u < pow (2, m); u++ ) {
+		for ( u = 0; u < powerFunction (2, m); u = u + split ) {
+			for ( j = u; j < u + split; j++ ) {
 
-			if ( u < ( pow (2, m) / 2 ) ) { 
-				printf ("\n[u]: %d, [u+tempPower]: %d, f[u]: %d, f[u+tempPower]: %d\n", u, u+tempPower, f[u], f[u + tempPower]);
-				temp_f[u] =  f[u] + f[ ( u + tempPower ) ] ; 
+				/* if the current  index is less than the split value, it adds the boolean values */
+				if ( j < tempSplit ) 
+					temp_f[j] =  f[j] + f[ ( j + split ) % powerFunction (2, m) ] ; 
+
+				/* if the current index is greater than the split value, it takes the difference 
+				 * of the boolean values */
+				else 
+					temp_f[j] = f [ j - split ] - f[j]; 
 			}
 
-			else {
-				printf ("\n[u]: %d, [u-tempPower]: %d, f[u-tempPower]: %d, F[u]: %d \n", u, u-tempPower, f[u - tempPower], f[u]);
-				temp_f[u] = f [ ( u - tempPower) % power ] - f[u]; 
+			/* Re-assigns the temp split value after two iterations 
+			 * i.e after completing one round of addition operations (f[u] + f[u + split ]
+			 * and difference operation (f[u] - f[u - split]) */
+			count = count + 1;
+
+			if (count == 2) {
+				tempSplit = tempSplit + powerFunction (2, split);
+				count = 0;
 			}
 		}
-
-		for ( u = 0; u < pow (2, m); u++) {
+		
+		/* Prints the boolean values at each stages */
+		for ( u = 0; u < powerFunction (2, m); u++ ) {
 			f[u] = temp_f[u];
 			printf ("%d\t", f[u]);
+
+			if ( u == ( powerFunction (2, m) - 1 ) )
+				printf ("\n");
 		}
 
-		printf ("\n");
 		temp = temp - 1;
 	}
+	free (temp_f);
 }
 
 int main ( int argc, char ** argv ) {
@@ -61,7 +106,7 @@ int main ( int argc, char ** argv ) {
 		printf ("Enter the value of [%d] of boolean funciton: ", i + 1);
 		scanf  ("%d", &f[i]);
 	}
-
 	FastWalshTransform (f, m);
+	free (f);
 	return 0;
 }
