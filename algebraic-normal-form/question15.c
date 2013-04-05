9f29f186e59addaeefad13b2012686fcae92bc08
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-05 14:17:13 +0530

    Added hamming weight function

--- algebraic-normal-form/question15.c
+++ algebraic-normal-form/question15.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* A function to calculate the hamming weight */
+int hamming_weight ( unsigned int M ) {
+	int count = 0;
+
+	while ( M > 0 ) {
+		if ( ( M & 1 ) == 1 )
+			count = count + 1;
+
+		M = M >> 1;
+	}
+	return count;
+}
+
 int main ( int argc, char ** argv ) {
 	return 0;
 }
