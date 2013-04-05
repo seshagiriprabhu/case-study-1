eba614accbd1c46b7f46ff1f0ebd7c5e92029e54
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 00:28:54 +0530

    Changed the function name to hamming_weight

--- binary-vectors/question3.c
+++ binary-vectors/question3.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
-unsigned int count_non_zero ( unsigned int u ) {
+unsigned int hamming_weight ( unsigned int u ) {
 	unsigned int count = 0;
 
 	while ( u > 0 ) {
@@ -43,7 +43,7 @@ int main ( int argc, char ** argv ) {
 					if ( u == 0 )
 						printf ("There isn't any non-zero values in your binary vector\n");
 					else
-						printf ("The Number of 1s in your binary vector is : %d\n", count_non_zero(u));
+						printf ("The Number of 1s in your binary vector is : %d\n", hamming_weight(u));
 			break;
 
 			case 3: printf ( "==========================================\n");
