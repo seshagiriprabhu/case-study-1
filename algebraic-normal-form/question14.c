11c2a834f3f6634322f5d8e88ced88eb81813d63
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-05 11:37:22 +0530

    Added power function

--- algebraic-normal-form/question14.c
+++ algebraic-normal-form/question14.c
@@ -5,6 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* A function to find the power of a given number */
+int powerFunction ( int number, int power ) {
+	if ( power == 0 )
+		return 0;
+	else 
+		return number << ( power - 1 );
+}
+
+/* A function to implement the monomial degree */
+int monomial_degree ( unsigned int * M ) {
+	int i, count = 0;
+
+	for ( i = 0; i < sizeof (M); i++ ) {
+
+	}
+}
+
 int main ( int argc, char ** argv ) {
 	return 0;
 }
