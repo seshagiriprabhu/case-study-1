20f56fbbc54f5c399367816fcd33686bc401fa97
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-05 10:09:13 +0530

    Adding question15

---
+++ algebraic-normal-form/question15.c
@@ -0,0 +1,11 @@
+/* Written by giri
+ * An implememtation of a  function monomial_eval(M, x) which evaluates 
+ * a monomial M at point x ∈ (F2)^m */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char ** argv ) {
+	return 0;
+}
+
