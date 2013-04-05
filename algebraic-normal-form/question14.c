eb41b7085744d1688c047ee4210af2fc7dab09aa
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-05 10:09:01 +0530

    Adding question14

---
+++ algebraic-normal-form/question14.c
@@ -0,0 +1,11 @@
+/* Written by giri
+ * Implementation of monomial_degree(M) which computes the degree of a 
+ * monomial given as an integer  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char ** argv ) {
+	return 0;
+}
+
