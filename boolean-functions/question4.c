09f901a95c75d2f9a9ee597e4f3ac978c87301dc
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 07:18:24 +0530

    removed hamming weight function and renamed calculate total hamming weight

--- boolean-functions/question4.c
+++ boolean-functions/question4.c
@@ -2,31 +2,20 @@
    A function taking as input a truthtable (as an array 
    of ints), and returning its Hamming weight */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "../header/header.h" /* FIX THIS make it to #include<header.h> */
 
 /* A custom function to find the hamming weight of a passed argument */
 // It is function count_non_zero from question 3! (with a better name...)
 // search DRY on google/wikipedia
-int hamming_weight (unsigned int u ) {
-	int count = 0;
-	while ( u > 0 ) {
-		if ( ( u & 1 ) == 1 ) {
-			count = count + 1;
-		}
-		u = u >> 1;
-	}
-	return count;
-}
 
 /* A function to calculate the hamming weight of each and every boolean value */
 // better name: e.g. table_hamming_weight
-int calculate_total_hamming_weight ( unsigned int * val ) {
+int table_hamming_weight ( unsigned long * val, int m ) {
 	int i, total_weight = 0;
 
         // Why only 4 values?? the example in the pdf has 4 values,
         // but your function should be general (to be usable in the real world)
-	for ( i = 0; i < 4; i++ ) {
+	for ( i = 0; i < pow2 (m); i++ ) {
             // why add hamming_weight(val[i])? what values can be in val[i]?
 		total_weight += hamming_weight(val[i]);
 	}
@@ -34,12 +23,19 @@ int calculate_total_hamming_weight ( unsigned int * val ) {
 }
 
 int main ( int argc, char ** argv ) {
-	unsigned int table_f[4];
+	unsigned long * table_f;
 	int i;
-	for ( i = 0; i < 4; i++) {
+	int m; /* Number of variables in the boolean function */
+
+	printf ("Enter the number of variables in the boolean function: ");
+	scanf ("%d", &m);
+	
+	table_f = ( unsigned long * ) malloc ( pow2 (m) * sizeof ( unsigned long *) );
+
+	for ( i = 0; i < pow2 (m); i++) {
 		printf ("Enter the %d(st/nd/rd/th) boolean value: ", i + 1);
-		scanf ("%du", &table_f[i]);
+		scanf ("%lu", &table_f[i]);
 	}
-	printf ("The hamming weight of the given boolean function is : %d\n", calculate_total_hamming_weight(table_f));
+	printf ("The hamming weight of the given boolean function is : %d\n", table_hamming_weight(table_f, m));
 	return 0;
 }
