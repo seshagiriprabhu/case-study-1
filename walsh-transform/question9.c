37ff2b1a7db21c3f1a091033db4cd200ed59a8f2
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 19:17:15 +0530

    Removed completeWalshTransform, improved intendation, made m as the variable of the boolean function

--- walsh-transform/question9.c
+++ walsh-transform/question9.c
@@ -6,9 +6,7 @@
  * size 2m containing all the values)
 */
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include "header.h"
 
 /* A function to calculate the power of a number */
 
@@ -18,21 +16,10 @@
 // In the context of Boolean functions,
 // the term "power function" means something
 // completely different
-int powerFunction ( int number, int power ) {
-	if ( power == 0 ) 
-		return 0;
-	else
-		return ( number << ( power - 1 ) );
-}
-
-/* A function to invert the sign of given boolean value */
 
 // I don't really get what this function is meant for...
 // But it has the same name as a different function
 // in question8.c, so it is confusing
-int sign (int e) {
-	return powerFunction ( (float) -1, e);
-}
 
 
 // Next 2 functions: what is m? there is confusion
@@ -41,69 +28,58 @@ int sign (int e) {
 // Also: don't do I/O in functions which should be pure!
 // Do I/O in e.g. main.
 
-void WalshTransform (int * f, int m) {
-	int i;
-	printf ("Walsh transform (f^(a) = Sigma (-1) ^ f(x)) of a boolean function f: \n");
-	for ( i = 0; i < m; i++ ) {
-		printf ("%d\n", sign(f[i]));
+UL WalshTransform ( UL *f, UL *a, UL m ) {
+	UL i, scalarProduct, output = 0;
+	for ( i = 0; i < pow2 (m); i++ ) {
+		scalarProduct = i * a[i];
+		scalarProduct = scalarProduct % 2;
+		output = output + (f[i] + scalarProduct); 
 	}
+	return output;
 }
 
-void completeWalshTransform ( int * f, int * a, int m) {
-	int i;
-	printf ("Complete walsh transform (f^(a) = Sigma (-1)^(f(x) + a.x) of a boolean function f:\n");
+int main () {
+	UL *table_f1;
+	UL *a;
+	UL m, i, j;
 
-	for ( i = 0; i < m; i++ ) 
-		printf ("%d\n", sign (f[i] + ( a[i] * i)));
-}
+	printf ("Enter the number of variables in the boolean function: ");
+	scanf ("%lu", &m);
 
-int main ( int argc, char ** argv ) {
-	int * table_f1;
-	int * a;
-	int m, i, j;
-
-	printf ("Enter the value of m: ");
-	scanf ("%d", &m);
-
-	table_f1 = ( int * ) malloc ( m * sizeof (int) );
-	a =  ( int * ) malloc ( m * sizeof (int) );
+	table_f1 = ( UL * ) malloc ( pow2 (m) * sizeof (UL) );
+	a =  ( UL * ) malloc ( pow2 (m) * sizeof (UL) );
 	
-	printf ("You are going to enter the values of the boolean function f\n");
-	for ( i = 0; i < m; i++ ) {
-		printf ("Enter the [%d] boolean value of boolean function f: ", i + 1);
-		scanf ("%d", &table_f1[i]);
-
-		if ( table_f1[i] > 1 )
-			table_f1[i] = 1;
-
-		if ( table_f1[i] < 0 )
-			table_f1[i] = 0;
+	for ( i = 0; i < pow2 (m); i++ ) {
+		printf ("Enter the [%lu] boolean value of boolean function f: ", i);
+		scanf ("%lu", &table_f1[i]);
+		if ( table_f1[i] != 0 && table_f1[i] != 1 ) {
+			printf ("Program accepts only binary values [0/1]\n");
+			i = i - 1;
+		}
 	}
 
-	printf ("You are going to enter the values of boolean function a\n");
-	for ( i = 0; i < m; i++ ) {
-		printf ("Enter the [%d] boolean value of boolean function a: ", i + 1);
-		scanf ("%d", &a[i]);
-
-		if ( a[i] > 1 )
-			a[i] = 1;
-		if ( a[i] < 0 )
-			a[i] = 0;
+	for ( i = 0; i < pow2 (m); i++ ) {
+		printf ("Enter the [%lu] boolean value of boolean function a: ", i);
+		scanf ("%lu", &a[i]);
+		if ( a[i] != 0 && a[i] != 1 ) {
+			printf ("Program accepts only binary values [0/1]\n");
+			i = i - 1;
+		}
 	}
 
 	printf ("Truth Table\n");
 	printf ("f\ta\n");
 
-	for ( i = 0; i < m; i++ ) {
+	for ( i = 0; i < pow2 (m); i++ ) {
 		for ( j = 0; j < 2; j++ ) {
 			if ( j == 0 ) 
-				printf ("%d\t", table_f1[i]);			
+				printf ("%lu\t", table_f1[i]);			
 
 			else 
-				printf ("%d\n", a[i]);
+				printf ("%lu\n", a[i]);
 		}	
 	}
-	WalshTransform (table_f1, m);
-	completeWalshTransform (table_f1, a, m);
+
+	printf ("The walsh transform of the given vector is %lu\n", WalshTransform (table_f1, a, m));
 	return 0;
 }
