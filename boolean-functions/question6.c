cbe3e03d01abbf417903470a13fe501691adba57
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 12:54:20 +0530

    Converted all the variable to unsigned long and included header.h

--- boolean-functions/question6.c
+++ boolean-functions/question6.c
@@ -5,22 +5,10 @@
    is_balanced(f) which checks the balancedness of a Boolean 
    function f */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
 
 /* A function to calculate the hamming weight */
 // DRY! we need to organize code in order to DRY
-int hamming_weight (unsigned value) {
-	int count = 0;
-
-	while ( value > 0 ) {
-		if ( ( value & 1 ) == 1 ) 
-			count = count + 1;
-		value = value >> 1;
-	}
-
-	return count;
-}
 
 /* A function which check whether the boolean functions are balanced
  * or not */
@@ -31,44 +19,48 @@ int hamming_weight (unsigned value) {
 // (boolean (true/false) value in this case).
 // after all, you don't scanf the values of f and m from
 // inside the function...
-void is_balanced (unsigned int * f, int m) {
-	int i, count = 0;
+UL is_balanced (UL * f, UL m) {
+	UL i, count = 0;
 
-	for ( i = 0; i < m; i++) {
+	for ( i = 0; i < pow2 (m); i++) {
             // same remark as before. It could be right if you
             // had optimized the representation of a truthtable,
             // using all bits of all values of the array f...
-		count += hamming_weight (f[i]);
+		if ( f[i] == 1 )
+			count = count + 1;
 	}
-
-	if ( count != ( m / 2 ) )
-		printf ("The given boolean function is not balanced\n");
-
-	else
-		printf ("The given boolean function is balanced\n");
+	return count;
 }
 
-int main ( int argc, char ** argv ) {
-	unsigned int * f;
-	int m, i;
+int main () {
+	UL * f;
+	UL m, i, count;
 	printf ("Enter the value of m: ");
-	scanf ("%d", &m);
+	scanf ("%lu", &m);
 	
 	if ( m == 0) {
 		printf ("You must atleast give a value\n");
 		exit(0);
 	}
 
-	f = ( unsigned int * ) malloc ( m * sizeof ( unsigned int ) );
+	f = ( UL * ) malloc ( m * sizeof ( UL ) );
 
+	for ( i = 0; i < pow2 (m); i++ ) {
+            // I don't think you should write 'i+1' ..
+		printf ("Enter the [%lu] boolean value of the boolean function: ", i);
+		scanf ("%lu", &f[i]);
 
-	for ( i = 0; i < m; i++ ) {
-            // I don't think you should write 'i+1' ...
-		printf ("Enter the value of bool[%d]: ", i + 1);
-		scanf ("%du", &f[i]);
+		if ( f[i] != 0 && f[i] != 1 ) {
+			printf ("Only boolean values [0/1] is allowed\n");
+			i = i - 1;
+		}	
 	}
 
-	is_balanced (f, m);
+	count = is_balanced (f, m);
+	if ( count == ( pow2 (m) / 2 ) )
+		printf ("The given boolean function is balanced\n");
+	else
+		printf ("The given boolean function is not balanced\n");
 
 	return 0;
 }
