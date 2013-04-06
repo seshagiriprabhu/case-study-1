9107d7e96583bbe67fb892198053325bf0ea5379
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 12:43:13 +0530

    Removed the unwanted blank spaces

--- binary-vectors/question2.c
+++ binary-vectors/question2.c
@@ -13,8 +13,8 @@
 // whereas (unsigned) long is 64 bits (native ints), so
 // long is preferred.
 
-int find_indice ( unsigned long u) {
-	int indice = 0;
+UL find_indice ( unsigned long u) {
+	UL indice = 0;
 
 	if ( u == 0 )
 		return 0;
@@ -26,7 +26,6 @@ int find_indice ( unsigned long u) {
 	while ( u > 0 ) {
 		if ( ( u  & 1 ) == 1 ) 
 			break;
-
 		indice = indice + 1;
 		u = u >> 1;
 	}
@@ -50,10 +49,8 @@ int main () {
 		switch ( choice ) {
 			case 1: printf ("Enter the integer: ");
 					scanf ("%lu", &u);
-
 					if ( u > 0 )
-						printf ("The indice of the first non-zero number is %d \n", find_indice (u));
-
+						printf ("The indice of the first non-zero number is %lu \n", find_indice (u));
 					else
 						printf ("The given unsigned integer doesn't contains a non-zero value\n");
 			break;
@@ -66,6 +63,7 @@ int main () {
 			break;
 		}
 	} while ( ! ( choice < 1 || choice > 2 ) );
+
 	printf ("=====================================================\n");
 	return 0;
 }
