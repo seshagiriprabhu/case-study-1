c371043adbebe4cf26ec51dd149c80cd2d89ae93
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 12:47:29 +0530

    Included sanitization checking and removed unwanted spaces

--- boolean-functions/question4.c
+++ boolean-functions/question4.c
@@ -4,19 +4,18 @@
 
 #include "header.h"
 /* UL is the macro of unsigned long */
-/* A custom function to find the hamming weight of a passed argument */
+
 // It is function count_non_zero from question 3! (with a better name...)
 // search DRY on google/wikipedia
 
 /* A function to calculate the hamming weight of each and every boolean value */
 // better name: e.g. table_hamming_weight
-UL table_hamming_weight ( UL * val, int m ) {
+UL table_hamming_weight ( UL *val, int m ) {
 	UL i, total_weight = 0;
 
         // Why only 4 values?? the example in the pdf has 4 values,
         // but your function shoULd be general (to be usable in the real world)
 	for ( i = 0; i < pow2 (m); i++ ) {
-            // why add hamming_weight(val[i])? what values can be in val[i]?
 		if ( val[i] == 1 )
 			total_weight  = total_weight + 1;
 	}
@@ -24,7 +23,7 @@ UL table_hamming_weight ( UL * val, int m ) {
 }
 
 int main ( ) {
-	UL * table_f;
+	UL *table_f;
 	UL i;
 	UL m; /* Number of variables in the boolean function */
 
@@ -34,10 +33,15 @@ int main ( ) {
 	table_f = ( UL * ) malloc ( pow2 (m) * sizeof ( UL *) );
 
 	for ( i = 0; i < pow2 (m); i++) {
-		printf ("Enter the %lu(st/nd/rd/th) boolean value: ", i + 1);
+		printf ("Enter the [%lu] boolean value: ", i);
 		scanf ("%lu", &table_f[i]);
+		if ( table_f[i] != 0 && table_f[i] != 1 ) { 
+			printf ("Program accepts only boolean values [0/1]\n");
+			i = i - 1;
+		}
 	}
 
 	printf ("The hamming weight of the given boolean function is : %lu\n", table_hamming_weight(table_f, m));
+	free(table_f);
 	return 0;
 }
