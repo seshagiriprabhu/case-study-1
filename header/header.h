90d37b60f09d6308c13b84a1d1852f17a1c3210b
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-06 18:10:38 +0530

    Added sign and scalar product function

--- header/header.h
+++ header/header.h
@@ -24,11 +24,27 @@ UL hamming_weight ( unsigned long u ) {
 	return count;
 }
 
+/* A function to calculate the power of 2 */
 UL pow2 ( int power ) {
 	if ( power == 0 )
 		return 1;
 
 	return 2 << (power - 1);
+}
+
+/* A function to invert the sign of a binary */
+int sign ( int e ) {
+	if ( e & 1 )
+		return -1;
+	return 0;
+}
+
+/* A function to calculate the scalar product of two vectors */
+UL scalar_product ( UL *table_f1, UL *table_f2, UL m ) {
+	UL i, product = 0;
+	for ( i = 0; i < pow2 (m); i++ ) 
+		product = product + ( table_f1[i] * table_f2[i] );
+	return product;
 }
 
 #endif
