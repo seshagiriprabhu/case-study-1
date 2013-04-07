5bb2b336a04d501c2b900a7f34c366030afe4656
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-07 18:54:41 +0530

    Made the FastWalshTransform function return an array

--- fast-walsh-transform/question11.c
+++ fast-walsh-transform/question11.c
@@ -8,21 +8,20 @@
 // a new array containing the walsh transform
 // (or better: which takes an array as a parameter where it puts
 // the walsh transform values).
-void FastWalshTransform ( int * f, int m ) {
-	int * temp_f;
+int * FastWalshTransform ( int *f, int m ) {
+	int *temp_f;
+	int count = 0, temp = m;
+	UL u, j;
+	UL split, tempSplit;
 	temp_f = ( int * ) malloc ( pow2 (m) * sizeof(int) );
-	int u, j, temp = m, count = 0;
-	int split, tempSplit;
 
 	/* Replaces all the values of the function by their sign */
 	for ( u = 0; u < pow2 (m); u++ ) 
 		temp_f[u] = f[u] = sign(f[u]);
-	temp = temp - 1;
 
+	temp = temp - 1;
 	while ( temp > -1 ) {	
-            // split is then equal to pow2(m), right??
-		split = pow2 (m) / ( pow2 (m) / pow2 (m) );
-		tempSplit = split;
+		tempSplit = split = pow2 (temp) ;
                 // this algo below, with tempSplit and count, and with
                 // "% pow2 (m)", seems over-complicated. There is a simpler solution.
 		for ( u = 0; u < pow2 (m); u = u + split ) {
@@ -34,7 +33,7 @@ void FastWalshTransform ( int * f, int m ) {
 
 				/* if the current index is greater than the split value, it takes the difference 
 				 * of the boolean values */
-				else 
+				else  
 					temp_f[j] = f [ j - split ] - f[j]; 
 			}
 
@@ -42,32 +41,46 @@ void FastWalshTransform ( int * f, int m ) {
 			 * i.e after completing one round of addition operations (f[u] + f[u + split ]
 			 * and difference operation (f[u] - f[u - split]) */
 			count = count + 1;
-
 			if (count == 2) {
-				tempSplit = tempSplit + pow2 (m);
+				tempSplit = tempSplit + pow2 (split);
 				count = 0;
 			}
 		}
+
+		for ( u = 0; u < pow2 (m); u++ )
+			f[u] = temp_f[u];
+
 		temp = temp - 1;
 	}
 	free (temp_f);
+	return temp_f;
 }
 
 int main ( ) {
-	int * f;
-	int m, i;
+	int *f;
+	int *fastWalsh_f;
+	UL m, i;
 
-	printf ("Enter the value of m : ");
-	scanf ("%d", &m);
+	printf ("Enter the number of variables in your boolean function : ");
+	scanf ("%lu", &m);
 
 	f = ( int * ) malloc ( pow2 (m) * sizeof ( int ) );
+	fastWalsh_f = ( int * ) malloc ( pow2 (m) * sizeof ( int ) );
 
 	for ( i = 0; i < pow2 (m); i++ ) {
-		printf ("Enter the value of [%d] of boolean funciton: ", i + 1);
+		printf ("Enter the value of [%lu] of boolean funciton: ", i);
 		scanf  ("%d", &f[i]);
 	}
 
-	FastWalshTransform (f, m);
+	fastWalsh_f = FastWalshTransform (f, m);
+
+	for ( i = 0; i < pow2 (m); i++ ) {
+		if ( i == 0 )
+			printf ("\n%d\t", fastWalsh_f[i]);
+		else
+			printf ("%d\t", fastWalsh_f[i]);
+	}
+	printf ("\n");
 	free (f);
 	return 0;
 }
