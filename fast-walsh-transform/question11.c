3111d484a876093866c36e06b30d69b57c7b4b7b
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-07 17:30:00 +0530

    Changed the arguments of pow2 function

--- fast-walsh-transform/question11.c
+++ fast-walsh-transform/question11.c
@@ -10,27 +10,27 @@
 // the walsh transform values).
 void FastWalshTransform ( int * f, int m ) {
 	int * temp_f;
-	temp_f = ( int * ) malloc ( pow2 (2, m) * sizeof(int) );
+	temp_f = ( int * ) malloc ( pow2 (m) * sizeof(int) );
 	int u, j, temp = m, count = 0;
 	int split, tempSplit;
 
 	/* Replaces all the values of the function by their sign */
-	for ( u = 0; u < pow2 (2, m); u++ ) 
+	for ( u = 0; u < pow2 (m); u++ ) 
 		temp_f[u] = f[u] = sign(f[u]);
 	temp = temp - 1;
 
 	while ( temp > -1 ) {	
-            // split is then equal to pow2(2, temp), right??
-		split = pow2 (2, m) / ( pow2 (2, m) / pow2 (2, temp) );
+            // split is then equal to pow2(m), right??
+		split = pow2 (m) / ( pow2 (m) / pow2 (m) );
 		tempSplit = split;
                 // this algo below, with tempSplit and count, and with
-                // "% pow2 (2, m)", seems over-complicated. There is a simpler solution.
-		for ( u = 0; u < pow2 (2, m); u = u + split ) {
+                // "% pow2 (m)", seems over-complicated. There is a simpler solution.
+		for ( u = 0; u < pow2 (m); u = u + split ) {
 			for ( j = u; j < u + split; j++ ) {
 
 				/* if the current  index is less than the split value, it adds the boolean values */
 				if ( j < tempSplit ) 
-					temp_f[j] =  f[j] + f[ ( j + split ) % pow2 (2, m) ] ; 
+					temp_f[j] =  f[j] + f[ ( j + split ) % pow2 (m) ] ; 
 
 				/* if the current index is greater than the split value, it takes the difference 
 				 * of the boolean values */
@@ -44,7 +44,7 @@ void FastWalshTransform ( int * f, int m ) {
 			count = count + 1;
 
 			if (count == 2) {
-				tempSplit = tempSplit + pow2 (2, split);
+				tempSplit = tempSplit + pow2 (m);
 				count = 0;
 			}
 		}
@@ -53,16 +53,16 @@ void FastWalshTransform ( int * f, int m ) {
 	free (temp_f);
 }
 
-int main ( int argc, char ** argv ) {
+int main ( ) {
 	int * f;
 	int m, i;
 
 	printf ("Enter the value of m : ");
 	scanf ("%d", &m);
 
-	f = ( int * ) malloc ( pow2 (2, m) * sizeof ( int ) );
+	f = ( int * ) malloc ( pow2 (m) * sizeof ( int ) );
 
-	for ( i = 0; i < pow2 (2, m); i++ ) {
+	for ( i = 0; i < pow2 (m); i++ ) {
 		printf ("Enter the value of [%d] of boolean funciton: ", i + 1);
 		scanf  ("%d", &f[i]);
 	}
