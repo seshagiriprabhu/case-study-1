700f378b714642675b7a4e33a17e505d00da18a3
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-09 13:42:39 +0530

    Removed UL_binary function, made a as an integer llong to long

--- walsh-transform/question9.c
+++ walsh-transform/question9.c
@@ -7,57 +7,29 @@
 */
 
 #include "header.h"
-/* Converts the given Unsigned integer to a binary arrray of size 2**m 
- * such that it can be used for scalar product with boolean function a */
-ulong * ulong_to_binary_array ( ulong x, ulong m ) {
-	ulong i = 0, j; 
-	ulong *xBinary;
+/* STATUS : INCOMPLETE */
+
+long WalshTransform ( ulong *f, ulong a, ulong m ) {
+	ulong x, product;
+	long output = 0;
 	ulong n = 1ul << m;
-	xBinary = ( ulong * ) malloc ( n * sizeof ( ulong ));
-	while ( x > 0 ) {
-		if ( x & 1 ) 
-			xBinary[i] = 1;
-		else 
-			xBinary[i] = 0;
-		i = i + 1;
-		x = x >> 1;
-	}
-	for ( j = i; j < n; j++ )
-		xBinary[j] = 0;
-	return xBinary;
-}
 
-/* Return value is long long because it should hold values from -2**m to 2**m */
-// Why do we need long long?
-
-// you have to reread the question :
-// a is a binary vector, given as an integer...
-// you should rewrite scalarProduct for integers.
-
-llong WalshTransform ( ulong *f, ulong *a, ulong m ) {
-	llong i, scalarProduct;						/* Holds the result of scalarProduct between a and x) */
-	llong signResult; 								/* Holds the result of sign function (-1)**(f(x) + a.x) */
-	llong output = 0; 								/* Holds the result of sigma function */
-	for ( i = 0; i < pow2 (m); i++ ) {
-		scalarProduct = scalar_product (a, ulong_to_binary_array (i, m), m); 
-		scalarProduct = hamming_weight(scalarProduct) % 2;  /* Modulo 2 is performed */
-		signResult =  sign ( (f[i] + scalarProduct) % 2 );  /* Sign values can take only 0 and 1 that's why modulo 2*/
-		output = output + signResult;
+	for ( x = 0; x < n; x++ ) { 
+		product = integer_product (a, x); /* Defined in utils.c*/
+		output = output +  sign ( (f[x] + product) % 2 ); 
 	}
 	return output;
 }
 
 int main () {
 	ulong *table_f1;
-	ulong *a;
-	ulong m, i, j;
+	ulong a, m, i;
 
 	printf ("Enter the number of variables in the boolean function: ");
 	scanf ("%lu", &m);
 	ulong n = 1ul << m;
 
 	table_f1 = ( ulong * ) malloc ( n * sizeof (ulong) );
-	a =  ( ulong * ) malloc ( n * sizeof (ulong) );
 	
 	for ( i = 0; i < n; i++ ) {
 		printf ("Enter the [%lu] boolean value of boolean function f: ", i);
@@ -68,27 +40,9 @@ int main () {
 		}
 	}
 
-	for ( i = 0; i < n; i++ ) {
-		printf ("Enter the [%lu] boolean value of boolean function a: ", i);
-		scanf ("%lu", &a[i]);
-		if ( a[i] != 0 && a[i] != 1 ) {
-			printf ("Program accepts only binary values [0/1]\n");
-			i = i - 1;
-		}
-	}
+	printf ("Enter the value of a: ");
+	scanf ("%lu", &a);
 
-	printf ("Truth Table\n");
-	printf ("f\ta\n");
-
-	for ( i = 0; i < n; i++ ) {
-		for ( j = 0; j < 2; j++ ) {
-			if ( j == 0 ) 
-				printf ("%lu\t", table_f1[i]);			
-			else 
-				printf ("%lu\n", a[i]);
-		}	
-	}
-
-	printf ("The walsh transform of the given vector is %lld\n", WalshTransform (table_f1, a, m));
+	printf ("The walsh transform of the given vector is %ld\n", WalshTransform (table_f1, a, m));
 	return 0;
 }
