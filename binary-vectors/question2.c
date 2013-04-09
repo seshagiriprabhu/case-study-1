56fd1e0ad142c69e4d6f28a59d9272f3f83c10ce
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-09 21:23:43 +0530

    Changed the find_indice algorithm according to the comment

--- binary-vectors/question2.c
+++ binary-vectors/question2.c
@@ -7,34 +7,20 @@
 /* Function returns the indice of the first non-zero value
    in the given unsigned integer						*/
 ulong find_indice ( ulong u) {
+	if (u == 0ul)
+		return ULONG_MAX;
 	ulong indice = 0;
-        // this version does the same thing as before
-        // The next two lines (special case for 0 and 1) are not useful:
-        // check that the result is the same if you remove them
-	if ( u == 0 || u == 1)
-		return 0;
-	while ( u > 1 ) { // you changed 0 for 1... but you break the
-                          // loop if 'u&1', so the result is exactly the same
-                          // you could have written 'while (true)'
-		if ( u & 1 ) 
-			break;
+	while (!(u & 1)) {
 		indice = indice + 1;
 		u = u >> 1;
 	}
 	return indice;
 }
 
-// my question was:
-       // And what do you think about the fact that we have :
-       // find_indice(0) == find_indice(1) ?
-// Answer: not a good idea! so we need to give a special value
-// to find_indice(0). Intuitively it would be +infinity, agreed?
-
-
 // To represent it, we set it to ULONG_MAX. This value has the nice
 // property that ULONG_MAX+1UL == 0. So I propose the following function,
 // do check that it works (is it an optimal solution?) :
-
+// Yes, I agree!
 ulong proposed_find_indice(ulong u) {
     if (u == 0UL)
         return ULONG_MAX;
