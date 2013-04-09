f58f810f371b582163b45edafebec569f283a833
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-09 22:06:34 +0530

    Added declaration of allocate_table function

--- header/header.h
+++ header/header.h
@@ -18,6 +18,7 @@ typedef long long llong;
 ulong hamming_weight (ulong u);
 ulong pow2 (int power);
 int sign (ulong e);
+ulong * allocate_table ( ulong m );
 ulong scalar_product (ulong *table_f1, ulong *table_f2, ulong m);
 bool integer_product (ulong vector1, ulong vector2);
 int * walsh_transform (int *f, int m);
