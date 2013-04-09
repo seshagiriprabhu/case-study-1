794dc7fc303dedbe39273968d44bf3f7b69cdd41
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-09 22:36:15 +0530

    changed the return type of integer_product to ulong

--- header/header.h
+++ header/header.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <limits.h>
-
+#include <assert.h>
 /*------------------Macros--------------------*/
 typedef int bool;
 #define true 1
@@ -20,7 +20,7 @@ ulong pow2 (int power);
 int sign (ulong e);
 ulong * allocate_table ( ulong m );
 ulong scalar_product (ulong *table_f1, ulong *table_f2, ulong m);
-bool integer_product (ulong vector1, ulong vector2);
+ulong integer_product (ulong vector1, ulong vector2);
 int * walsh_transform (int *f, int m);
 ulong table_hamming_weight (ulong *val, ulong m);
 #endif
