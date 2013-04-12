fb047ea919acdd2843ee31fff39a2c02a60a881a
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-12 10:50:29 +0530

    Added monomial degree function declaration

--- header/header.h
+++ header/header.h
@@ -14,7 +14,7 @@ typedef int bool;
 
 typedef unsigned long ulong;
 typedef long long llong;
-
+/*-------------Function Declarations----------*/
 ulong hamming_weight (ulong u);
 ulong pow2 (int power);
 int sign (ulong e);
@@ -24,4 +24,5 @@ ulong scalar_product (ulong *table_f1, ulong *table_f2, ulong m);
 ulong integer_product (ulong vector1, ulong vector2);
 long * walsh_transform (long *f, ulong m);
 ulong table_hamming_weight (ulong *val, ulong m);
+ulong monomial_degree (ulong M);
 #endif
