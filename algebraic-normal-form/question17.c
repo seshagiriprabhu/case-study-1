e542d1f0bce616c2dcf405101e6e17dcb30e3d54
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-13 14:52:10 +0530

    Adding question17 code

---
+++ algebraic-normal-form/question17.c
@@ -0,0 +1,25 @@
+/* Written by Giri
+ * An implementation of ANF_eval function */
+
+#include "header.h"
+/* STATUS : INCOMPLETE */
+
+int main () {
+	ulong m, u, x;
+	ulong *A; 
+	printf ("Enter the number of variables in your boolean function: ");
+	scanf ("%lu", &m);
+	A = allocate_table (m);
+	ulong n = 1UL << m;
+	for ( u = 0; u < n; u++ ) {
+		printf ("Enter the [%lu] boolean value: ", u);
+		scanf ("%lu", &A[u]);
+	}
+	printf ("Enter the integer (x) which you want to evaluate: ");
+	scanf ("%lu", &x);
+	if ( ANF_eval (A, x, m) )
+		printf ("%lu is an integer of the given ANF\n", x);
+	else
+		printf ("%lu is not an integer of the given ANF\n", x);
+	return 0;
+}
