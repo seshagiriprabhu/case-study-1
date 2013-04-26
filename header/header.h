4d3fa96df4fd6cb68af62dd592f14c2164896374
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-26 22:11:21 +0530

    changed the names of functions

--- header/header.h
+++ header/header.h
@@ -20,14 +20,14 @@ bool check_random_bit (ulong bit, ulong i);
 ulong hamming_weight (ulong u);
 ulong pow2 (int power);
 int sign (ulong e);
+ulong scalar_product_table (ulong *table_f1, ulong *table_f2, ulong m);
+ulong table_hamming_weight (ulong *val, ulong m);
 ulong * allocate_table (ulong m);
 long * allocate_long_table (ulong m);
-ulong scalar_product (ulong *table_f1, ulong *table_f2, ulong m);
-ulong integer_product (ulong vector1, ulong vector2);
+ulong scalar_product (ulong vector1, ulong vector2);
 long * walsh_transform (long *f, ulong m);
-ulong table_hamming_weight (ulong *val, ulong m);
 ulong monomial_degree (ulong M);
 bool monomial_eval (ulong M, ulong x);
-ulong ANF (ulong *M, ulong m);
+ulong ANF_degree (ulong *M, ulong m);
 bool ANF_eval (ulong *A, ulong x, ulong m);
 #endif
