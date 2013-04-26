90faf1f82a989b2b4662d32f8fc71045efdcd44f
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-26 22:12:28 +0530

    Added changes according to the previous comments

--- header/util.c
+++ header/util.c
@@ -33,7 +33,7 @@ int sign ( ulong e ) {
 }
 
 /* A function to calculate the scalar product of two vectors */
-ulong scalar_product ( ulong *table_f1, ulong *table_f2, ulong m ) {
+ulong scalar_product_table ( ulong *table_f1, ulong *table_f2, ulong m ) {
 	ulong i, product = 0;
 	ulong n = 1UL << m;
 	for ( i = 0; i < n; i++ ) 
@@ -60,31 +60,10 @@ long * allocate_long_table ( ulong m ) {
 	return ( ( long * ) malloc ( ( 1UL << m ) * sizeof (long) ));
 }
 
-// integer_product ? really it should be named scalar_product
-// (and rename your scalar_product above to scalar_product_table?)
-
-/* A function to calculate the product of two boolean vectors 
- * given as integers */
-ulong integer_product ( ulong a, ulong x ) {
-	ulong result = 0;
-        // it is enough that only one is null to break the loop
-	while ( x != 0UL || a != 0UL ) {
-            // if we want to do as few tests as possible (for more efficiency... to check!):
-            // if ( (x&a) & 1UL)
-		if ( x & 1UL && a & 1UL )	
-			result = result + 1;
-                // or even directly (instead of the two lines above) : result += x&a&1UL
-		a = a >> 1UL; // no need here to write 1UL instead of 1.
-		x = x >> 1UL;	
-	}
-	return result;
-}
-
-/* Another approach to the integer product */
-// YES good!
-ulong integer_product_alternative ( ulong a, ulong x ) {
-	ulong result = a & x;
-	return hamming_weight (result);
+/* A function to calculate the scalar product of two 
+ * boolean vectors given as integers */
+ulong scalar_product( ulong a, ulong x ) {
+	return hamming_weight (a&x);
 }
 
 /* A function to computer the FastWalshTransform 
@@ -115,35 +94,16 @@ ulong monomial_degree ( ulong M ) {
 	return hamming_weight (M);	
 }
 
-// It looks a lot like integer_product... can't you use your optimization
-// of integer_product_alternative?
-
 /* A function to evaluate a Monomial */
 bool monomial_eval ( ulong M, ulong x ) {
-	ulong count = 0, degree = hamming_weight (M);
-	while ( M != 0UL && x != 0UL ) {
-		if ( M & 1 && x & 1 )
-			count++;
-		x = x >> 1;
-		M = M >> 1;
-	}
-	if ( count == degree )
+	if ( hamming_weight(M&x) >= hamming_weight(M) )
 		return 1;
 	return 0;
 }
 
-/* A function to compute ANF */
-// Your comment (and name of function) is misleading: this function
-// computes the degree of the ANF M, so it should be called AMF_degree
-ulong ANF (ulong *M, ulong m) {
+/* A function to compute ANF degree */
+ulong ANF_degree (ulong *M, ulong m) {
 	ulong u, tempMax = 0, index = 0, n = 1UL << m;
-        // ulong *monomial;
-        // monomial = allocate_table (m);
-        // ^^^ there is really no need to allocate a table of monomials: you
-        // only use monomial[index] ... so I remove it
-        // moreover, you implemented monomial_degree: it is a trivial function,
-        // but in C it incurs 0 overhead to call a function which can be inlined as this one.
-        // So use monomial_degree as it better expresses our intent
 	for ( u = 0; u < n; u++) {
 		if ( M[u] & 1 ) {
 			if ( tempMax < monomial_degree (u) )
@@ -154,27 +114,13 @@ ulong ANF (ulong *M, ulong m) {
 	return tempMax;
 }
 
+/* A function to evaluate ANF */
 bool ANF_eval (ulong *A, ulong x, ulong m) {
-	ulong u, i = 0, output = 0, n = 1UL << m;
-	ulong *index, *monomial;
-        // it is very confising that index and monomial are equal
-	monomial = index = allocate_table (m);
+	ulong u, val = 0, n = 1UL << m;
 	for ( u = 0; u < n; u++ ) {
-		if ( A[u] & 1 )
-			index[i] = u;
-			i = i + 1;
+		if ( A[u])
+			val ^= monomial_eval(u, x);
 	}
-	for ( u = 0; u < i; u++ ) 
-                // why assign monomial_eval(...) to monomial[u] ??
-		output += monomial[u] = monomial_eval (index[u], x);
-	return output % 2;
+	return val;
 }	
 
-// so a simpler version (do you agree?) :
-bool ANF_eval_2(ulong *A, ulong m, ulong x) {
-    ulong val = 0, u, n=1ul<<m;
-    for (u=0; u<n; ++u)
-        if (A[u])
-            val ^= monomial_eval(u, x);
-    return val;
-}
